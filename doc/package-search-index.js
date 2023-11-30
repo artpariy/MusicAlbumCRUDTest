4494c576ea4fcc95dc87ac4b71f8299cdc614fd8
artpariy <art.pariy@gmail.com>
2023-11-30 21:19:59 +0300

    Add JavaDoc and maven pom.xml

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.pariy.albums.menu"},{"l":"ru.pariy.albums.model"},{"l":"ru.pariy.albums.repository"},{"l":"ru.pariy.albums.service"}]
